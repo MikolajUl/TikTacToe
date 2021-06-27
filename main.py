35e3c89684589be2d42951f54adea6d4ad19f891
Mikolaj Ulatowski <77579890+MikolajUl@users.noreply.github.com>
2021-06-27 19:01:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def check_win(modified_possible_wins, player_move, player):
     for position in range(len(modified_possible_wins) - 2):
         # Third requirement divides list into chunks of 3
         if modified_possible_wins[position] == modified_possible_wins[position + 1] and \
-           modified_possible_wins[position] == modified_possible_wins[position + 2] and\
-           (position + 3) % 3 == 0:
+                modified_possible_wins[position] == modified_possible_wins[position + 2] and \
+                (position + 3) % 3 == 0:
             return True
 
 
@@ -35,10 +35,8 @@ table = """
 3  ~  |  ~  |  ~  
       |     | """
 
-# Indexes in string of squares able to fill found by :
+# Indexes in string of squares able to fill, found by :
 # indexes = [index for index, sign in enumerate(table) if sign == "~"]
-
-
 positions_to_fill = {
     "A1": 43,
     "A2": 100,
@@ -55,18 +53,15 @@ positions_to_fill = {
 win_scenarios = [["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"], ["A1", "B1", "C1"],
                  ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A1", "B2", "C3"], ["A3", "B2", "C1"]]
 
-
 #        ___Gameplay___
 
-# Player1 being X or O
 player1 = input("Pick X or O : ")
 player1 = player1.capitalize()
 
-# Check if input is valid: X or O
+# Check if input is valid
 if player1 != "X" and player1 != "O":
     raise TypeError("Wrong pick")
 
-# Player2 becomes opposite to player one
 if player1 == "X":
     player2 = "O"
 else:
@@ -78,8 +73,6 @@ for scenario in win_scenarios:
     for element in scenario:
         auxiliary_list.append(element)
 
-
-# Looping while nobody won
 win = False
 current_player = player1
 
@@ -91,16 +84,13 @@ while not win:
     # Reformatting input by capitalizing first letter
     move = replace(move, move[0].capitalize(), 0)
 
-    # Checking if move is valid choice
     if move in positions_to_fill:
-
         # Updating table with current_player's choice
         table = replace(table, current_player, positions_to_fill[move])
 
         # Remove used position from positions_to_fill
         positions_to_fill.pop(move)
 
-        # Win detection
         if check_win(auxiliary_list, move, current_player):
             print(table)
             print(f"\nGAME WON BY {current_player}")
